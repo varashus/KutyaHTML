58e4c87624cbe04af2cf02f66b3c075c8981d286
varashus <115972590+varashus@users.noreply.github.com>
2024-03-20 10:25:00 +0100

    asdasdasd
    asdasdas

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const expandableImgs = document.querySelectorAll('.expandable-img');
-
-    expandableImgs.forEach(img => {
-        img.addEventListener('click', () => {
-            img.classList.toggle('expanded');
-        });
-    });
-});
